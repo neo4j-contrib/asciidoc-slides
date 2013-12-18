9e4dbb2e843afa5293716366579523d51bbd6002
Michael Hunger <github@jexp.de>
2013-12-18 02:34:15 +0100

    more content for the online training, in demo show how to graph-gist w/o header for multiple slides

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a stylesdir=`PWD`/asciidoc/deck.js/themes/style "$@"
+asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a stylesdir=`PWD`/asciidoc/deck.js/themes/style -a allow-uri-read "$@"
