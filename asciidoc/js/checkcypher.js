7170c9abc924be3333c3b0ecd8657fc0d09636fe
Peter Neubauer <peter@neubauer.se>
2014-03-21 13:50:43 +0100

    not showing GOTO by default, adding menu

--- asciidoc/js/checkcypher.js
+++ asciidoc/js/checkcypher.js
@@ -53,7 +53,6 @@ function setupDiagrams() {
 function init() {
 
     setupDiagrams();
-    $.deck('showGoTo')
     var $status = $("#status");
     $status.text(status);
     var cypherblocks = [];
