8841c614d7e5f65db601fd8d1425795dd9e7a333
Pieter Cailliau <pieter.cailliau@neotechnology.com>
2015-04-28 14:54:27 +0100

    run sets copyright year automatically

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a stylesdir=`pwd`/asciidoc/deck.js/themes/style -a allow-uri-read "$@"
+current_year=$(date "+%Y")
+asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a stylesdir=`pwd`/asciidoc/deck.js/themes/style -a allow-uri-read -a currentyear=$current_year "$@"
