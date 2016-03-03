0e54a00bf9789ccf3bd1cab6dc699a7dec91c59a
Sven Janko <sven.janko@neotechnology.com>
2016-03-03 13:59:50 +0100

    save name, twitter and email in file for later use; minor shell improvements

--- create_training_slides.sh
+++ create_training_slides.sh
@@ -1,31 +1,56 @@
 git submodule init
 git submodule update
 
+# save name, twitter and email of the presenter in this file
+PRESENTER_INFO_FILE=presenter.info
+
 ADOC_VERSION=$(asciidoctor -v)
 if [ ! $? ];  then
 	echo "Installing AsciiDoctor"
 	bundle install
 else
 	echo "Installed"
-	echo $ADOC_VERSION
+	echo "$ADOC_VERSION"
 fi
 
-echo -n "Your Name: "
-read name
-echo -n "Your Twitter: "
-read twitter
-echo -n "Your Email: "
-read email
+function writePresenterInfo {
+  cat << EOF > "$PRESENTER_INFO_FILE"
+name="$name"
+twitter="$twitter"
+email="$email"
+EOF
+}
+
+function readPresenterInfo {
+
+	# try to read it from file first
+	if [ -r "$PRESENTER_INFO_FILE" ]; then
+		echo "Reading name, twitter and email from file: '$PRESENTER_INFO_FILE'"
+		source "$PRESENTER_INFO_FILE"
+	else
+
+		echo -n "Your Name: "
+		read name
+		echo -n "Your Twitter: "
+		read twitter
+		echo -n "Your Email: "
+		read email
+
+		writePresenterInfo
+	fi
+}
+
+readPresenterInfo
 
 function create {
-	./run.sh content/training/$1 -a presenter="$name" -a twitter="$twitter" -a email="$email"
+	./run.sh "content/training/$1" -a presenter="$name" -a twitter="$twitter" -a email="$email"
 }
 SLIDES="advanced_cypher/training-advanced-cypher.adoc import/training-data-import.adoc modeling/training-data-modeling.adoc production/training-neo4j-in-production.adoc intro/intro-cypher-movies/training-intro-cypher-movies.adoc intro/intro-cypher-relational/training-intro-cypher-relational.adoc intro/intro-cypher-interactive/training-intro-cypher-interactive.adoc intro/graph-days-movies/training-graph-days-cypher-movies.adoc"
 
 
 for i in $SLIDES; do
 	echo "Rendering $i"
-	create $i
+	create "$i"
 done
 
 ./http index.html
