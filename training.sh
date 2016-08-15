15ad4fcae7739aa7c6c636195dfb87292cd00ada
Michael Hunger <github@jexp.de>
2016-08-15 13:09:34 +0200

    fixed error in build script

--- training.sh
+++ training.sh
@@ -3,5 +3,6 @@ file=${1-intro/intro-cypher-movies/training-intro-cypher-movies.adoc}
 presenter=${2-"Neo4j"}
 email=${3-"info@neotechnology.com"}
 twitter=${4-"neo4j"}
+twitter=${twitter#@*}
 
-./run.sh content/training/$file -a presenter=$presenter -a email=$email -a twitter=$twitter
+./run.sh content/training/$file -a presenter="$presenter" -a email="$email" -a twitter="$twitter"
