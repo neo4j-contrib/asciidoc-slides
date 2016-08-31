0bf8bb986f09da3f8fe2e2633a46ab8941062083
Michael Hunger <github@jexp.de>
2016-08-31 02:12:41 +0200

    no toc for pdfs

--- newpdf.sh
+++ newpdf.sh
@@ -1,1 +1,1 @@
-asciidoctor-pdf -d book -a env-pdf -a pdf-page-size=A5 -a pdf-style=slide-pdf.yml -a sectids $*
+asciidoctor-pdf -d book -a toc! -a env-pdf -a pdf-page-size=A5 -a pdf-style=slide-pdf.yml -a sectids $*
