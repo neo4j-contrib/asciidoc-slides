60c711d4913c63df516bd4dbc1c854b23c29ebcc
Michael Hunger <github@jexp.de>
2016-09-08 23:46:26 +0200

    add utf-8 to rendering

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 current_year=$(date "+%Y")
 echo Building $@
-asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a stylesdir=`pwd`/asciidoc/deck.js/themes/style -a allow-uri-read -a currentyear=$current_year "$@"
+asciidoctor -T asciidoc/asciidoctor-backends/haml/deckjs -a encoding=UTF-8 -a stylesdir=`pwd`/asciidoc/deck.js/themes/style -a allow-uri-read -a currentyear=$current_year "$@"
